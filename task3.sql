c4b47ba9db4a8f01f3f23b888bdceabfb770b3c8
Your Name <you@example.com>
2023-11-27 22:25:37 +0100

    first commit

---
+++ task3.sql
@@ -0,0 +1,25 @@
+-- Вывести самый дорогой товар из категории 4
+
+SELECT *
+FROM products
+WHERE CategoryID = 4
+ORDER BY Price DESC
+LIMIT 1;
+
+-- Вывести страны поставщиков, кроме поставщика 1
+
+
+SELECT DISTINCT Country
+FROM suppliers
+WHERE SupplierID <> 1;
+
+-- Вывести клиентов из Germany, имена которых начинаются на t
+
+SELECT *
+FROM Customers
+WHERE Country = "Germany" AND CustomerName LIKE "T%";
+
+-- Вывести телефоны перевозчиков
+
+SELECT Phone
+FROM shippers;
